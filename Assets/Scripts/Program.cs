bbf730bade0a965b7439991429cba57146422cf6
Alex Webb <alexwebbb@protonmail.com>
2017-09-27 19:30:20 -0500

    first commit

---
+++ Assets/Scripts/Program.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Program : MonoBehaviour {
+
+    public bool debugMode = false;
+
+	// Use this for initialization
+	void Start () {
+        Reset();
+	}
+
+
+    private void Reset() {
+        
+        
+
+
+
+    }
+}
