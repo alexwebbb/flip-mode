7458658f613869f6891a22244e3be6dcd93db4ca
Alex Webb <alexwebbb@protonmail.com>
2017-09-27 23:31:49 -0500

    playin around with bottle flip

---
+++ Assets/Scripts/ForceTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceTest : MonoBehaviour
+{
+    public Vector3 force = Vector3.zero;
+    public Vector3 torque = Vector3.zero;
+    Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(force);
+            transform.Rotate(torque);
+            Debug.Log("test");
+        }
+
+    }
+}
